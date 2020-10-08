59e9b6dcbcc571792ebbc53dfad560c857e74eff
DariaGorbach <43273648+DariaGorbach@users.noreply.github.com>
2020-10-08 15:51:37 +0300

    Update pea_scrpt.R

--- pea_scrpt.R
+++ pea_scrpt.R
@@ -2,7 +2,7 @@ library("Biostrings")
 library(stringr)
 library(dplyr)
 
-# читаю базу данных 
+# Pisum_sativum_v1a_prot - фаста файл белковых последовательностей 
 pea_db <- readBStringSet("C:/Users/admin/Pisum_sativum_v1a_prot.fasta")
 pea_names = names(pea_db)
 sequence = paste(pea_db)
@@ -10,6 +10,7 @@ pea_db <- data.frame(pea_names, sequence)
 pea_db$sequence <- as.character(pea_db$sequence)
 pea_db$pea_names <- tolower(pea_db$pea_names)
 
+#Pisum_sativum_genome_names.csv - названия белков по номенклатуре Uniprot
 pea_prot_names <- read.csv("C:/Users/admin/Pisum_sativum_genome_names.csv", header = T)
 pea_prot_names <- pea_prot_names[, 2:3]
 colnames(pea_prot_names)[1] <- "pea_names" 
@@ -21,6 +22,7 @@ pea_db_test <- pea_db_test[, c(3,2,1)]
 writeFasta(data = pea_db_test, filename = "Pea_proteome.fasta")
 
 #######
+#Pea_coat_comp.csv - таблица с данными по диффэкспресии
 pea_table <- read.csv("C:/Users/admin/Pea_coat_comp.csv", header = T, sep = ";", dec = ",")
 colnames(pea_table)[12:21] <- c("wild_076", "wild_077", "wild_078", "wild_079", "wild_080", "cult_081", "cult_082", "cult_083", "cult_084", "cult_085")
 pea_table$first_accession <- gsub(pea_table$Accession, pattern = ";.*", replacement = "")
